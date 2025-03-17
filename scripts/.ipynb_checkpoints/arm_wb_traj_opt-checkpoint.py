63a54e948f873c25529ead13de011b0ab4081d35
Jiayi Wang <jjiayu889@gmail.com>
2025-03-17 14:31:13 +0800

    sync

---
+++ scripts/.ipynb_checkpoints/arm_wb_traj_opt-checkpoint.py
@@ -0,0 +1,7 @@
+from math import pi
+
+import casadi
+from pinocchio import casadi as cpin
+from ipywidgets import interact
+from example_robot_data import load
+from pinocchio.visualize import MeshcatVisualizer
